f826f8842e4a39a8a74f49f3c3eec544b4a706de
slavyanHristov <slavqnhristov@gmail.com>
2021-05-01 16:30:19 +0300

    Testing git pull and git fetch

--- CSSystem/src/main/java/cssystem/Controller.java
+++ CSSystem/src/main/java/cssystem/Controller.java
@@ -1,29 +1,24 @@
 package cssystem;
 
-<<<<<<< HEAD
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javafx.scene.control.TextField;
 
 public class Controller {
     private EntityManager entityManager;
     private EntityManagerFactory factory;
+    private TextField usernameField = null;
+
+    public void initialize() {
+        usernameField.setText("Set username: ");
+
+    }
 
     public Controller(EntityManager entityManager, EntityManagerFactory factory) {
         this.entityManager = entityManager;
         this.factory = factory;
     }
 
-    public void putka(){
-
-=======
-import javafx.scene.control.TextField;
-
-
-public class Controller {
-    private TextField usernameField = null;
-
-    public void initialize(){
-        usernameField.setText("Set username: ");
->>>>>>> 7a4aee3 (testing git pull)
+    public void putka() {
     }
 }
