8539e278162b5a723a5188e55563e1979f342e38
KaloyanANDSlavyan <slaviikoko@gmail.com>
2021-05-01 16:12:34 +0300

    xd

--- CSSystem/src/main/java/cssystem/Controller.java
+++ CSSystem/src/main/java/cssystem/Controller.java
@@ -1,4 +1,18 @@
 package cssystem;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 public class Controller {
+    private EntityManager entityManager;
+    private EntityManagerFactory factory;
+
+    public Controller(EntityManager entityManager, EntityManagerFactory factory) {
+        this.entityManager = entityManager;
+        this.factory = factory;
+    }
+
+    public void putka(){
+
+    }
 }
