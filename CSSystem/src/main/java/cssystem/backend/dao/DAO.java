bbf0728813af5adc6cc1703680433d750ca3375e
KaloyanANDSlavyan <slaviikoko@gmail.com>
2021-05-08 16:52:55 +0300

    Adding work.
    Update use case and add authentication service

--- CSSystem/src/main/java/cssystem/backend/dao/DAO.java
+++ CSSystem/src/main/java/cssystem/backend/dao/DAO.java
@@ -1,6 +1,7 @@
 package cssystem.backend.dao;
 
-public interface DAO<T> {
+public interface DAO<T, V> {
     void save(T object);
     T findByID(Class<T> c, Long id);
+    T findBy2Values(Class<T> c, String column1, String column2, V value1, V value2);
 }
